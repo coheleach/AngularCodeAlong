c0158170895a945fb32a68c7c62f7a07a693a439
Brennan Guy Coheleach <Brennan.Coheleach1@gmail.com>
2020-05-13 18:19:47 -0400

    Connecting Ingredients HTML with FormGroup in typescript

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipesService } from '../recipes.service';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -13,6 +13,10 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   id: number;
   recipeForm: FormGroup;
+
+  get ingredientsAsControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 
   constructor(private recipesService: RecipesService,
               private activatedRoute: ActivatedRoute) { }
@@ -33,18 +37,28 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let description = '';
+    let recipeIngredients = new FormArray([]);
 
     if(this.editMode) {
       const recipe = this.recipesService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.filePath;
       description = recipe.description;
+      if(recipe.ingredients) {
+        for(let ingredient of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(ingredient.name),
+            'amount': new FormControl(ingredient.amount)
+          }))
+        }
+      }
     }
     
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(description)
+      'description': new FormControl(description),
+      'ingredients': recipeIngredients
     });
   }
 
