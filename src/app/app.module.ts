7fbcd3fa8ac17f29c8a7d5708889d52f84522bb3
Brennan Guy Coheleach <Brennan.Coheleach1@gmail.com>
2020-06-01 14:19:51 -0400

    added new authorization component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { AuthComponent } from './auth/auth.component';
 
 import { DropdownDirective } from './Shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -34,7 +35,8 @@ import { RecipesResolverService } from './recipes/recipes-resolver.service';
     ShoppingEditComponent,
     DropdownDirective,
     SelectRecipeComponent,
-    RecipeEditComponent,   
+    RecipeEditComponent,
+    AuthComponent
     ],
   imports: [
     BrowserModule,
