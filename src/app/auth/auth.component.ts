2caf7a5aae899373204d668bcf5b25628d6d7ffc
Brennan Guy Coheleach <Brennan.Coheleach1@gmail.com>
2020-06-02 16:06:42 -0400

    added Sign in functionality to auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-auth',
@@ -19,26 +20,26 @@ export class AuthComponent {
     }
 
     onSubmit(form: NgForm) {
-
+        
         console.log(form.value);
         this.isLoading = true;
+        let signupLoginObservable: Observable<any>;
         if(this.isLoginMode) {
-            // ...
+            signupLoginObservable = this.authService.login(form.value.email, form.value.password)    
         } else {
-            this.authService.signUp(form.value.email, form.value.password)
-                .subscribe(responseBody => {
-                    console.log(responseBody);
-                }, error => {
-                    console.log(error);
-                    this.error = error.error.error;
-                })
+            signupLoginObservable = this.authService.signUp(form.value.email, form.value.password)
         }
-        
+        this.signupLoginSubscription(signupLoginObservable);
         this.isLoading = false;
         form.reset();
     }
 
-    onConfirmError() {
-        this.error = null;
+    private signupLoginSubscription(observable: Observable<any>) {
+        observable.subscribe(responseBody => {
+            console.log(responseBody);
+        }, errorMessage => {
+            console.log(errorMessage);
+            this.error = errorMessage;
+        });
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { pipe, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 class SignupResponsePayload {
     idToken: string
@@ -7,6 +9,15 @@ class SignupResponsePayload {
     refreshToken: string
     expiresIn: string
     localId: string
+}
+
+class SignInResponsePayload  {
+    idToken: string
+    email: string
+    refreshToken: string
+    expiresIn: string
+    localId: string
+    registered: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -22,6 +33,43 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        );
+        )
+        .pipe(catchError(error => {
+            let errorMessage = 'An unknown error ocurred';
+            if(!error.error || !error.error.error) {
+                return throwError(errorMessage);
+            }
+            switch(error.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'The email address is already in use by another account.';
+                    break;
+            }
+            return throwError(errorMessage);
+        }))
+    }
+
+    login(email: string, password: string) {
+        return this.httpClient.post<SignInResponsePayload>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBJRCutoN10m5ocS35VMGem-SJv6cW62r4',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
+        .pipe(catchError(error => {
+            let errorMessage = 'An unknown error ocurred';
+            if(!error.error || !error.error.error) {
+                return throwError(errorMessage);
+            }
+            switch(error.error.error.message) {
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = 'No account was found with this email';
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = 'Incorrect Password';
+            }
+            return throwError(errorMessage);
+        }))
     }
 }
